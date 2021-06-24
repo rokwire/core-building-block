60ee950784bd66c520dbd842eae35bb58521a633
Stefan Vitanov <stefan@inabyte.com>
2021-06-24 09:44:10 +0300

    done. For review

--- driver/web/adminapis.go
+++ driver/web/adminapis.go
@@ -2,6 +2,9 @@ package web
 
 import (
 	"core-building-block/core"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -27,7 +30,29 @@ func NewAdminApisHandler(app *core.Application) AdminApisHandler {
 	return AdminApisHandler{app: app}
 }
 
+type createGlobalConfigRequest struct {
+	Setting string `json:"setting"`
+	Name    string `json:"name"`
+}
+
 //CreateGlobalConfig creates a global config
 func (h AdminApisHandler) CreateGlobalConfig(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error on marshal create global config - %s\n", err.Error())
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
 
+	var requestData createGlobalConfigRequest
+	err = json.Unmarshal(data, &requestData)
+	if err != nil {
+		log.Printf("Error on unmarshal the create global config data - %s\n", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Successfully created"))
 }
