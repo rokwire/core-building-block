e102ab32415c08e471d4362d7fa748e37c62ebe6
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-07-05 10:18:07 +0300

    Use mockery + test packages - in progress

--- core_test/administration_test.go
+++ core_test/administration_test.go
@@ -1,3 +1,53 @@
 package core_test
 
-//TODO
+import (
+	"testing"
+
+	core "core-building-block/core"
+	core_model "core-building-block/core/model"
+	genmocks "core-building-block/mocks"
+
+	"gotest.tools/assert"
+)
+
+func TestAdmGetTest(t *testing.T) {
+	storage := genmocks.Storage{}
+	app := core.NewApplication("1.1.1", "build", &storage, nil)
+
+	got := app.Administration.AdmGetTest()
+	want := "Admin - test"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestAdmCreateGlobalConfig(t *testing.T) {
+	storage := genmocks.Storage{}
+	storage.On("CreateGlobalConfig", "setting").Return(&core_model.GlobalConfig{"setting"}, nil)
+
+	app := core.NewApplication("1.1.1", "build", &storage, nil)
+
+	gc, _ := app.Administration.AdmCreateGlobalConfig("setting")
+	if gc == nil {
+		t.Error("gc is nil")
+		return
+	}
+	if gc.Setting != "setting" {
+		t.Errorf("got %q, wanted %q", gc.Setting, "setting")
+		return
+	}
+	assert.Equal(t, gc.Setting, "setting", "setting is different")
+
+	/*	//second case - error
+		storage2 := genmocks.Storage{}
+		app = core.NewApplication("1.1.1", "build", &storage2, nil)
+
+		_, err := app.Administration.AdmCreateGlobalConfig("setting")
+		if err == nil {
+			t.Error("we are expecting error")
+			return
+		}
+		assert.Equal(t, err.Error(), "error occured", "error is different") */
+
+}
--- core_test/application_test.go
+++ core_test/application_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestAddListener(t *testing.T) {
 	storage := genmocks.Storage{}
-	core.NewApplication("1.1.1", "build", &storage, nil)
+	app := core.NewApplication("1.1.1", "build", &storage, nil)
 	/*
 		listeners := app.listeners
 		if len(listeners) != 0 {
 			t.Error("listeners is not empty")
 		}
 	*/
-	/*	app.AddListener(ListenerMock{})
-
+	app.AddListener(genmocks.ApplicationListener{})
+	/*
 		listeners = app.listeners
 		if len(listeners) != 1 {
 			t.Error("listeners must has 1 listener")
