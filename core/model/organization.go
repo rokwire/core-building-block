a19388d352b82f1a642e2e05c5a3864f026a39ed
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-08-02 08:32:45 +0300

    Do not remove the Application entity struct

--- core/model/organization.go
+++ core/model/organization.go
@@ -76,3 +76,12 @@ func (cur OrganizationUserRelations) String() string {
 	return fmt.Sprintf("[ID:%s\n\tType:%s\n\tRelationManager:%s\n\tMembers:%s\n\t]",
 		cur.ID, cur.Type, cur.Manager, cur.Members)
 }
+
+//Application represents users application entity - safer community, uuic, etc
+type Application struct {
+	ID       string
+	Name     string   //safer community mobile, safer community web, uuic mobile, uuic web, uuic admin etc
+	Versions []string //1.1.0, 1.2.0 etc
+
+	Organizations []Organization
+}
