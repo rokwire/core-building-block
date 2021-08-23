789adcef7b71cfb40111bd96fe59418c9380d543
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-08-23 06:12:40 +0300

    Add TODO comment

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -39,6 +39,7 @@ const (
 )
 
 //TODO biometrics handling
+//TODO Register - for regular and externals(OIDC happens automatically)..
 
 //Auth represents the auth functionality unit
 type Auth struct {
