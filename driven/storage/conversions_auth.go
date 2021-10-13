e0d00d59892b15a8fda6359f5600026ffc460cf9
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-10-13 13:33:54 +0300

    Implement conversion function

--- driven/storage/conversions_auth.go
+++ driven/storage/conversions_auth.go
@@ -5,7 +5,21 @@ import "core-building-block/core/model"
 //LoginSession
 func loginSessionToStorage(item *model.LoginSession) *loginSession {
 	id := item.ID
-	//TODO
+	anonymous := item.Anonymous
+	identifier := item.Identifier
+	var accountAuthTypeID *string
+	if item.AccountAuthType != nil {
+		accountAuthTypeID = &item.AccountAuthType.ID
+	}
+	deviceID := item.Device.ID
+	IP := item.IP
+	accessToken := item.AccessToken
+	refreshToken := item.RefreshToken
+	params := item.Params
+	expires := item.Expires
+	dateCreated := item.DateCreated
 
-	return &loginSession{ID: id}
+	return &loginSession{ID: id, Anonymous: anonymous, Identifier: identifier, AccountAuthTypeID: accountAuthTypeID,
+		DeviceID: deviceID, IP: IP, AccessToken: accessToken, RefreshToken: refreshToken, Params: params,
+		Expires: expires, DateCreated: dateCreated}
 }
