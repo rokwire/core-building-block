bcc10fcdae2b1451bcb4db4f59270737f6fc5cf1
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-10-13 11:19:04 +0300

    Add login session storage struct

---
+++ driven/storage/model_auth.go
@@ -0,0 +1,23 @@
+package storage
+
+import "time"
+
+type loginSession struct {
+	ID string `bson:"_id"`
+
+	Anonymous bool `bson:"anonymous"`
+
+	Identifier        string  `bson:"identifier"`
+	AccountAuthTypeID *string `bson:"account_auth_type_id"`
+
+	DeviceID string `bson:"device_id"`
+
+	IP           string      `bson:"ip"`
+	AccessToken  string      `bson:"access_token"`
+	RefreshToken string      `bson:"refresh_token"`
+	Params       interface{} `bson:"params"`
+
+	Expires time.Time `bson:"expires"`
+
+	DateCreated time.Time `bson:"date_created"`
+}
