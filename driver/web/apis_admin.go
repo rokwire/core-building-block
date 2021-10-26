739b23d0f35ecd701f274dc266e6744aa776ac34
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-10-26 06:42:06 +0300

    Fix build after merge

--- driver/web/apis_admin.go
+++ driver/web/apis_admin.go
@@ -38,164 +38,18 @@ func (h AdminApisHandler) getTestModel(l *logs.Log, r *http.Request, claims *tok
 	return l.HttpResponseSuccessMessage(res)
 }
 func (h AdminApisHandler) adminLogin(l *logs.Log, r *http.Request, claims *tokenauth.Claims) logs.HttpResponse {
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionRead, logutils.TypeRequestBody, nil, err, http.StatusBadRequest, false)
-	}
-
-	//get ip
 	//TODO
-	ip := r.RemoteAddr
-
-	var requestData Def.ReqLoginAdminRequest
-	err = json.Unmarshal(data, &requestData)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionUnmarshal, logutils.MessageDataType("auth login request"), nil, err, http.StatusBadRequest, true)
-	}
-
-	//creds
-	requestCreds, err := interfaceToJSON(requestData.Creds)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionMarshal, model.TypeCreds, nil, err, http.StatusBadRequest, true)
-	}
-
-	//params
-	requestParams, err := interfaceToJSON(requestData.Params)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionMarshal, "params", nil, err, http.StatusBadRequest, true)
-	}
-
-	//preferences
-	var requestPreferences map[string]interface{}
-	if requestData.Preferences != nil {
-		requestPreferences = *requestData.Preferences
-	}
-
-	//profile ////
-	requestProfile := profileFromDefNullable(requestData.Profile)
-
-	//device
-	requestDevice := requestData.Device
-
-	message, loginSession, err := h.coreAPIs.Auth.Login(ip, string(requestDevice.Type), requestDevice.Os, *requestDevice.DeviceId,
-		string(requestData.AuthType), requestCreds, requestData.AppTypeIdentifier, requestData.OrgId, requestParams, requestProfile, requestPreferences, l)
-	if err != nil {
-		return l.HttpResponseError("Error logging in", err, http.StatusInternalServerError, true)
-	}
-
-	///prepare response
-
-	//message
-	if message != nil {
-		responseData := &Def.ResLoginAdminResponse{Message: message}
-		respData, err := json.Marshal(responseData)
-		if err != nil {
-			return l.HttpResponseErrorAction(logutils.ActionMarshal, logutils.MessageDataType("auth login response"), nil, err, http.StatusInternalServerError, false)
-		}
-		return l.HttpResponseSuccessJSON(respData)
-	}
-
-	//token
-	accessToken := loginSession.AccessToken
-	refreshToken := loginSession.RefreshToken
-
-	tokenType := Def.ResSharedRokwireTokenTokenTypeBearer
-	rokwireToken := Def.ResSharedRokwireToken{AccessToken: &accessToken, RefreshToken: &refreshToken, TokenType: &tokenType}
-
-	//account
-	var accountData *Def.ResLoginAccount
-	if !loginSession.Anonymous {
-		account := loginSession.AccountAuthType.Account
-
-		//profile
-		profile := profileToDef(&account.Profile)
-		//preferences
-		preferences := &account.Preferences
-		//permissions
-		permissions := applicationPermissionsToDef(account.Permissions)
-		//roles
-		roles := applicationRolesToDef(account.Roles)
-		//groups
-		groups := applicationGroupsToDef(account.Groups)
-		//account auth types
-		authTypes := accountAuthTypesToDef(account.AuthTypes)
-		accountData = &Def.ResLoginAccount{Id: account.ID, Permissions: &permissions, Roles: &roles, Groups: &groups, AuthTypes: &authTypes, Profile: profile, Preferences: preferences}
-	}
-
-	//params
-	var paramsRes interface{}
-	if loginSession.Params != nil {
-		paramsRes = loginSession.Params
-	}
-
-	responseData := &Def.ResLoginResponse{Token: &rokwireToken, Account: accountData, Params: &paramsRes}
-	respData, err := json.Marshal(responseData)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionMarshal, logutils.MessageDataType("auth login response"), nil, err, http.StatusInternalServerError, false)
-	}
-
-	return l.HttpResponseSuccessJSON(respData)
+	return l.HttpResponseSuccess()
 }
 
 func (h AdminApisHandler) adminLoginURL(l *logs.Log, r *http.Request, claims *tokenauth.Claims) logs.HttpResponse {
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionRead, logutils.TypeRequestBody, nil, err, http.StatusBadRequest, false)
-	}
-
-	var requestData Def.ReqLoginUrlAdminRequest
-	err = json.Unmarshal(data, &requestData)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionUnmarshal, "auth login url request", nil, err, http.StatusBadRequest, true)
-	}
-
-	loginURL, params, err := h.coreAPIs.Auth.GetLoginURL(string(requestData.AuthType), requestData.AppTypeIdentifier, requestData.OrgId, requestData.RedirectUri, l)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionGet, "login url", nil, err, http.StatusInternalServerError, true)
-	}
-
-	responseData := &Def.ResLoginUrlAdminResponse{LoginUrl: loginURL, Params: &params}
-	respData, err := json.Marshal(responseData)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionMarshal, "auth login url response", nil, err, http.StatusInternalServerError, false)
-	}
-
-	return l.HttpResponseSuccessJSON(respData)
+	//TODO
+	return l.HttpResponseSuccess()
 }
 
 func (h AdminApisHandler) adminRefresh(l *logs.Log, r *http.Request, claims *tokenauth.Claims) logs.HttpResponse {
-	requestData, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionRead, logutils.TypeRequestBody, nil, err, http.StatusBadRequest, false)
-	}
-
-	reqToken := string(requestData)
-	loginSession, err := h.coreAPIs.Auth.Refresh(reqToken, l)
-	if err != nil {
-		return l.HttpResponseError("Error refreshing token", err, http.StatusInternalServerError, true)
-	}
-	if loginSession == nil {
-		//if login session is null then unauthorized
-		l.Infof("trying to refresh - %s", reqToken)
-		return l.HttpResponseError(http.StatusText(http.StatusUnauthorized), nil, http.StatusUnauthorized, true)
-	}
-
-	accessToken := loginSession.AccessToken
-	refreshToken := loginSession.RefreshToken
-	var paramsRes interface{}
-	if loginSession.Params != nil {
-		paramsRes = loginSession.Params
-	}
-
-	tokenType := Def.ResSharedRokwireTokenTokenTypeBearer
-	rokwireToken := Def.ResSharedRokwireToken{AccessToken: &accessToken, RefreshToken: &refreshToken, TokenType: &tokenType}
-	responseData := &Def.ResRefreshResponse{Token: &rokwireToken, Params: &paramsRes}
-	respData, err := json.Marshal(responseData)
-	if err != nil {
-		return l.HttpResponseErrorAction(logutils.ActionMarshal, logutils.MessageDataType("auth refresh response"), nil, err, http.StatusInternalServerError, false)
-	}
-
-	return l.HttpResponseSuccessJSON(respData)
+	//TODO
+	return l.HttpResponseSuccess()
 }
 
 //createGlobalConfig creates a global config
