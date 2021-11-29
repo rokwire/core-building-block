c6434693116c1d2715ebff60c670ddaa655dfe53
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2021-11-29 09:47:35 +0200

    Add mfa_ prefix to all mfa files

--- core/auth/email.go
+++ core/auth/email.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"core-building-block/core/model"
-	"core-building-block/driven/storage"
 	"core-building-block/utils"
 	"crypto/subtle"
 	"encoding/json"
@@ -10,7 +9,6 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/rokwire/logging-library-go/errors"
 	"github.com/rokwire/logging-library-go/logs"
 	"github.com/rokwire/logging-library-go/logutils"
@@ -20,8 +18,6 @@ import (
 const (
 	//AuthTypeEmail email auth type
 	AuthTypeEmail string = "email"
-	//MfaTypeEmail email mfa type
-	MfaTypeEmail string = "email"
 
 	typeTime        logutils.MessageDataType = "time.Time"
 	typeEmailCreds  logutils.MessageDataType = "email creds"
@@ -470,84 +466,6 @@ func initEmailAuth(auth *Auth) (*emailAuthImpl, error) {
 	err := auth.registerAuthType(email.authType, email)
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionRegister, typeAuthType, nil, err)
-	}
-
-	return email, nil
-}
-
-// Email implementation of mfaType
-type emailMfaImpl struct {
-	auth    *Auth
-	mfaType string
-}
-
-func (m *emailMfaImpl) verify(context storage.TransactionContext, mfa *model.MFAType, accountID string, code string) (*string, error) {
-	if mfa == nil || mfa.Params == nil {
-		return nil, errors.ErrorData(logutils.StatusMissing, "mfa params", nil)
-	}
-
-	var message string
-
-	storedCode, ok := mfa.Params["code"].(string)
-	if !ok {
-		return nil, errors.ErrorData(logutils.StatusInvalid, "stored mfa code", nil)
-	}
-	if code != storedCode {
-		message = "invalid code"
-		return &message, errors.ErrorData(logutils.StatusInvalid, "mfa code", nil)
-	}
-
-	expiry, ok := mfa.Params["expires"].(time.Time)
-	if !ok {
-		return nil, errors.ErrorData(logutils.StatusInvalid, "stored expiry", nil)
-	}
-	if time.Now().UTC().After(expiry) {
-		message = "expired code"
-		return &message, errors.ErrorData(logutils.StatusInvalid, "expired code", nil)
-	}
-
-	//remove code and expiration from params in storage
-	delete(mfa.Params, "code")
-	delete(mfa.Params, "expires")
-	err := m.auth.storage.UpdateMFAType(context, mfa, accountID)
-	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionUpdate, model.TypeMFAType, nil, err)
-	}
-
-	return nil, nil
-}
-
-func (m *emailMfaImpl) enroll(identifier string) (*model.MFAType, error) {
-	if identifier == "" {
-		return nil, errors.ErrorData(logutils.StatusMissing, "identifier", nil)
-	}
-
-	//TODO: check identifier is valid email address
-
-	params := map[string]interface{}{
-		"identifier": identifier,
-	}
-
-	id, _ := uuid.NewUUID()
-	return &model.MFAType{ID: id.String(), Type: MfaTypeEmail, Verified: false, Params: params, DateCreated: time.Now().UTC()}, nil
-}
-
-func (m *emailMfaImpl) sendCode(identifier string) (string, *time.Time, error) {
-	code := fmt.Sprintf("%06d", utils.GenerateRandomInt(mfaCodeMax))
-	expires := time.Now().Add(time.Duration(mfaCodeExpiration) * time.Minute)
-
-	//TODO: send code to identifier, store both in DB
-
-	return code, &expires, errors.New(logutils.Unimplemented)
-}
-
-//initEmailMfa initializes and registers a new email mfa instance
-func initEmailMfa(auth *Auth) (*emailMfaImpl, error) {
-	email := &emailMfaImpl{auth: auth, mfaType: MfaTypeEmail}
-
-	err := auth.registerMfaType(email.mfaType, email)
-	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionRegister, typeMfaType, nil, err)
 	}
 
 	return email, nil
--- core/auth/phone.go
+++ core/auth/phone.go
@@ -3,11 +3,9 @@ package auth
 import (
 	"context"
 	"core-building-block/core/model"
-	"core-building-block/driven/storage"
 	"core-building-block/utils"
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -15,7 +13,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/rokwire/logging-library-go/errors"
 	"github.com/rokwire/logging-library-go/logs"
 	"github.com/rokwire/logging-library-go/logutils"
@@ -25,8 +22,6 @@ import (
 const (
 	//AuthTypeTwilioPhone phone auth type
 	AuthTypeTwilioPhone string = "twilio_phone"
-	//MfaTypePhone phone mfa type
-	MfaTypePhone string = "phone"
 
 	servicesPathPart                                  = "https://verify.twilio.com/v2/Services"
 	verificationsPathPart                             = "Verifications"
@@ -300,84 +295,6 @@ func initPhoneAuth(auth *Auth, twilioAccountSID string, twilioToken string, twil
 	err := auth.registerAuthType(phone.authType, phone)
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionRegister, typeAuthType, nil, err)
-	}
-
-	return phone, nil
-}
-
-// Phone implementation of mfaType
-type phoneMfaImpl struct {
-	auth    *Auth
-	mfaType string
-}
-
-func (m *phoneMfaImpl) verify(context storage.TransactionContext, mfa *model.MFAType, accountID string, code string) (*string, error) {
-	if mfa == nil || mfa.Params == nil {
-		return nil, errors.ErrorData(logutils.StatusMissing, "mfa params", nil)
-	}
-
-	var message string
-
-	storedCode, ok := mfa.Params["code"].(string)
-	if !ok {
-		return nil, errors.ErrorData(logutils.StatusInvalid, "stored mfa code", nil)
-	}
-	if code != storedCode {
-		message = "invalid code"
-		return &message, errors.ErrorData(logutils.StatusInvalid, "mfa code", nil)
-	}
-
-	expiry, ok := mfa.Params["expires"].(time.Time)
-	if !ok {
-		return nil, errors.ErrorData(logutils.StatusInvalid, "stored expiry", nil)
-	}
-	if time.Now().UTC().After(expiry) {
-		message = "expired code"
-		return &message, errors.ErrorData(logutils.StatusInvalid, "expired code", nil)
-	}
-
-	//remove code and expiration from params in storage
-	delete(mfa.Params, "code")
-	delete(mfa.Params, "expires")
-	err := m.auth.storage.UpdateMFAType(context, mfa, accountID)
-	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionUpdate, model.TypeMFAType, nil, err)
-	}
-
-	return nil, nil
-}
-
-func (m *phoneMfaImpl) enroll(identifier string) (*model.MFAType, error) {
-	if identifier == "" {
-		return nil, errors.ErrorData(logutils.StatusMissing, "identifier", nil)
-	}
-
-	//TODO: check identifier is valid phone number
-
-	params := map[string]interface{}{
-		"identifier": identifier,
-	}
-
-	id, _ := uuid.NewUUID()
-	return &model.MFAType{ID: id.String(), Type: MfaTypePhone, Verified: false, Params: params, DateCreated: time.Now().UTC()}, nil
-}
-
-func (m *phoneMfaImpl) sendCode(identifier string) (string, *time.Time, error) {
-	code := fmt.Sprintf("%06d", utils.GenerateRandomInt(mfaCodeMax))
-	expires := time.Now().Add(time.Duration(mfaCodeExpiration) * time.Minute)
-
-	//TODO: send code to identifier, store both in DB
-
-	return code, &expires, errors.New(logutils.Unimplemented)
-}
-
-//initPhoneMfa initializes and registers a new phone mfa instance
-func initPhoneMfa(auth *Auth) (*phoneMfaImpl, error) {
-	phone := &phoneMfaImpl{auth: auth, mfaType: MfaTypePhone}
-
-	err := auth.registerMfaType(phone.mfaType, phone)
-	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionRegister, typeMfaType, nil, err)
 	}
 
 	return phone, nil
