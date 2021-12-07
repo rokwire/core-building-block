af63d37fdcccec804ed921af594254767757c630
stefanvit <75947010+stefanvit@users.noreply.github.com>
2021-12-07 07:46:19 +0200

    [ID-270]Expose get account admin API (#275)
    * Setting the Changelog.md
    
    * set def.yaml
    
    * fix def.yaml
    
    * in progress
    
    * fix def.yaml
    
    * move Account to response/shared
    
    * fix
    
    * Setting def.yaml
    
    * Set Changelog.md
    
    * set def.yaml
    
    * Setting the core admin account
    
    * setting the web for getAccount admin API
    
    * resolve conflicts
    
    * resolve comments
    
    * fix Changelog
    
    * resolve comments
    
    * Fix changelog
    
    * Do not change code not related with the issue
    
    * Do not change code not related with the issue - part 2
    
    * Remove wrong docs
    
    * Fix docs, expose API
    
    * Call CoreAPIs
    
    * Reuse code
    
    Co-authored-by: Stefan Vitanov <stefan@inabyte.com>
    Co-authored-by: Petyo Stoyanov <petyo.st.stoyanov@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Expose get account admin API [#270] (https://github.com/rokwire/core-building-block/issues/270)
+
 
 ## [1.17.0] - 2021-12-06
 ### Fixed
--- core/app.go
+++ core/app.go
@@ -1,5 +1,12 @@
 package core
 
+import (
+	"core-building-block/core/model"
+
+	"github.com/rokwire/logging-library-go/errors"
+	"github.com/rokwire/logging-library-go/logutils"
+)
+
 //application represents the core application code based on hexagonal architecture
 type application struct {
 	env     string
@@ -30,4 +37,13 @@ func (app *application) notifyListeners(message string, data interface{}) {
 	go func() {
 		// TODO
 	}()
+}
+
+func (app *application) getAccount(accountID string) (*model.Account, error) {
+	//find the account
+	account, err := app.storage.FindAccountByID(nil, accountID)
+	if err != nil {
+		return nil, errors.WrapErrorAction(logutils.ActionFind, model.TypeAccount, nil, err)
+	}
+	return account, nil
 }
