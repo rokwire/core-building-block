b99df29fdc94bc83b54308924bb8432cd04b8431
Stefan Vitanov <stefan@inabyte.com>
2022-01-04 08:03:37 +0200

    set auth

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -117,7 +117,7 @@ func (we Adapter) Start() {
 	adminSubrouter.HandleFunc("/account/mfa", we.wrapFunc(we.adminApisHandler.addMFAType, we.auth.admin.authenticated)).Methods("POST")
 	adminSubrouter.HandleFunc("/account/mfa", we.wrapFunc(we.adminApisHandler.removeMFAType, we.auth.admin.authenticated)).Methods("DELETE")
 
-	adminSubrouter.HandleFunc("/appOrgRoles", we.wrapFunc(we.adminApisHandler.adminDeleteApplicationOrgRoles, nil)).Methods("DELETE")
+	adminSubrouter.HandleFunc("/appOrgRoles", we.wrapFunc(we.adminApisHandler.adminDeleteApplicationOrgRoles, we.auth.admin.user)).Methods("DELETE")
 
 	adminSubrouter.HandleFunc("/application/accounts", we.wrapFunc(we.adminApisHandler.getApplicationAccounts, we.auth.admin.user)).Methods("GET")
 	///
