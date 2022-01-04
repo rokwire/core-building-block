9a2ea6fe2af3c571cec26d2e65a8b08d598d295d
Stefan Vitanov <stefan@inabyte.com>
2022-01-04 08:00:56 +0200

    set auth

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -110,7 +110,7 @@ func (we Adapter) Start() {
 	adminSubrouter.HandleFunc("/auth/login-url", we.wrapFunc(we.adminApisHandler.adminLoginURL, nil)).Methods("POST")
 	adminSubrouter.HandleFunc("/auth/refresh", we.wrapFunc(we.adminApisHandler.adminRefresh, nil)).Methods("POST")
 
-	adminSubrouter.HandleFunc("/appOrgRoles", we.wrapFunc(we.adminApisHandler.adminCreateApplicationOrgRoles, nil)).Methods("POST")
+	adminSubrouter.HandleFunc("/appOrgRoles", we.wrapFunc(we.adminApisHandler.adminCreateApplicationOrgRoles, we.auth.admin.user)).Methods("POST")
 
 	adminSubrouter.HandleFunc("/auth/verify-mfa", we.wrapFunc(we.adminApisHandler.adminVerifyMFA, we.auth.admin.user)).Methods("POST")
 	adminSubrouter.HandleFunc("/auth/app-token", we.wrapFunc(we.adminApisHandler.getAppToken, we.auth.admin.user)).Methods("GET")
