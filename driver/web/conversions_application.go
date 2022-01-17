1f4999c5e474dc417dafb7ba7651ae54975c1562
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2022-01-17 07:34:04 +0200

    Add 'System' field in the convertion function

--- driver/web/conversions_application.go
+++ driver/web/conversions_application.go
@@ -43,7 +43,7 @@ func appOrgRolesToDef(items []model.AppOrgRole) []Def.AppOrgRoleFields {
 
 //AppOrgGroup
 func appOrgGroupToDef(item model.AppOrgGroup) Def.AppOrgGroupFields {
-	return Def.AppOrgGroupFields{Id: item.ID, Name: item.Name}
+	return Def.AppOrgGroupFields{Id: item.ID, Name: item.Name, System: &item.System}
 }
 
 func appOrgGroupsToDef(items []model.AppOrgGroup) []Def.AppOrgGroupFields {
