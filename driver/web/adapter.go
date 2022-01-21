71900f2b447751eafe814191b7676bfbc5a3debf
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2022-01-21 10:56:48 +0200

    Apply authorization of the API

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -120,7 +120,7 @@ func (we Adapter) Start() {
 	adminSubrouter.HandleFunc("/organization/applications", we.wrapFunc(we.adminApisHandler.adminGetApplications, we.auth.admin.user)).Methods("GET")
 
 	adminSubrouter.HandleFunc("/application/groups", we.wrapFunc(we.adminApisHandler.adminGetApplicationGroups, we.auth.admin.permissions)).Methods("GET")
-	adminSubrouter.HandleFunc("/application/groups", we.wrapFunc(we.adminApisHandler.adminCreateApplicationGroups, we.auth.admin.user)).Methods("POST")
+	adminSubrouter.HandleFunc("/application/groups", we.wrapFunc(we.adminApisHandler.adminCreateApplicationGroups, we.auth.admin.permissions)).Methods("POST")
 
 	adminSubrouter.HandleFunc("/application/roles", we.wrapFunc(we.adminApisHandler.adminGetApplicationOrgRoles, we.auth.admin.permissions)).Methods("GET")
 	adminSubrouter.HandleFunc("/application/roles", we.wrapFunc(we.adminApisHandler.adminCreateApplicationRole, we.auth.admin.permissions)).Methods("POST")
