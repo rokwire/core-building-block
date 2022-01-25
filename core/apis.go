481be35d8c4d1e43dd94693f2a479b92d93b8fa3
Stefan Vitanov <stefan@inabyte.com>
2022-01-25 15:36:58 +0200

    fixing the core

--- core/apis.go
+++ core/apis.go
@@ -143,8 +143,8 @@ func (s *administrationImpl) AdmGetAccount(accountID string) (*model.Account, er
 	return s.app.admGetAccount(accountID)
 }
 
-func (s *administrationImpl) AdmGetApplicationLoginSessions(appID string, orgID string, identifier string) ([]model.LoginSession, error) {
-	return s.app.admGetApplicationLoginSessions(appID, orgID, identifier)
+func (s *administrationImpl) AdmGetApplicationLoginSessions(appID string, orgID string, identifier *string, accountAuthTypeIdentifier string) ([]model.LoginSession, error) {
+	return s.app.admGetApplicationLoginSessions(appID, orgID, identifier, accountAuthTypeIdentifier)
 }
 
 ///
