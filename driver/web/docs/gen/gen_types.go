8d88114681be4a28430cd39e2e4ba96c0bb33a35
Stefan Vitanov <stefan@inabyte.com>
2022-01-28 08:43:54 +0200

    fixing the docs

--- driver/web/docs/gen/gen_types.go
+++ driver/web/docs/gen/gen_types.go
@@ -934,6 +934,12 @@ type PostAdminAccountMfaParams struct {
 	Type string `json:"type"`
 }
 
+// GetAdminApplicationAccountDeviceParams defines parameters for GetAdminApplicationAccountDevice.
+type GetAdminApplicationAccountDeviceParams struct {
+	// The account ID
+	Id string `json:"id"`
+}
+
 // GetAdminApplicationAccountsParams defines parameters for GetAdminApplicationAccounts.
 type GetAdminApplicationAccountsParams struct {
 	// The account ID
