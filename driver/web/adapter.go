5bc88e53c1563b59cc0cd13d1c27bbb4b9aa041e
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2022-02-07 06:15:41 +0200

    Do not register the API two times

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -120,7 +120,6 @@ func (we Adapter) Start() {
 	adminSubrouter.HandleFunc("/account/mfa", we.wrapFunc(we.adminApisHandler.addMFAType, we.auth.admin.authenticated)).Methods("POST")
 	adminSubrouter.HandleFunc("/account/mfa", we.wrapFunc(we.adminApisHandler.removeMFAType, we.auth.admin.authenticated)).Methods("DELETE")
 
-	adminSubrouter.HandleFunc("/application/account/{account_id}/session/{session_id}", we.wrapFunc(we.adminApisHandler.deleteLoginSession, we.auth.admin.permissions)).Methods("DELETE")
 	adminSubrouter.HandleFunc("/organization/applications", we.wrapFunc(we.adminApisHandler.adminGetApplications, we.auth.admin.user)).Methods("GET")
 
 	adminSubrouter.HandleFunc("/application/login-sessions", we.wrapFunc(we.adminApisHandler.getApplicationLoginSessions, we.auth.admin.permissions)).Methods("GET")
@@ -136,7 +135,8 @@ func (we Adapter) Start() {
 	adminSubrouter.HandleFunc("/application/permissions", we.wrapFunc(we.adminApisHandler.getApplicationPermissions, we.auth.admin.permissions)).Methods("GET")
 
 	adminSubrouter.HandleFunc("/application/accounts", we.wrapFunc(we.adminApisHandler.getApplicationAccounts, we.auth.admin.permissions)).Methods("GET")
-	adminSubrouter.HandleFunc("/application/account/{account_id}/session/{session_id}", we.wrapFunc(we.adminApisHandler.deleteLoginSession, we.auth.admin.user)).Methods("DELETE")
+
+	adminSubrouter.HandleFunc("/application/account/{account_id}/session/{session_id}", we.wrapFunc(we.adminApisHandler.deleteLoginSession, we.auth.admin.permissions)).Methods("DELETE")
 	///
 
 	///enc ///
