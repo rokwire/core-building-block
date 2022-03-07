460056737df6ffadcc4508fd6dcea6ab3860fd3c
Ryan Oberlander <reoberlander2@gmail.com>
2022-03-07 17:14:04 -0600

    start defining service account params endpoints

--- driver/web/adapter.go
+++ driver/web/adapter.go
@@ -154,6 +154,7 @@ func (we Adapter) Start() {
 	bbsSubrouter.HandleFunc("/service-regs", we.wrapFunc(we.bbsApisHandler.getServiceRegistrations, nil)).Methods("GET") //Public
 	bbsSubrouter.HandleFunc("/test", we.wrapFunc(we.bbsApisHandler.getTest, nil)).Methods("GET")                         //Public
 
+	// bbsSubrouter.HandleFunc("/service-account", we.wrapFunc(we.bbsApisHandler.getServiceAccountParams, nil)).Methods("POST") //Public
 	bbsSubrouter.HandleFunc("/access-token", we.wrapFunc(we.bbsApisHandler.getServiceAccessToken, nil)).Methods("POST") //Public
 	///
 
@@ -162,6 +163,7 @@ func (we Adapter) Start() {
 	tpsSubrouter.HandleFunc("/service-regs", we.wrapFunc(we.tpsApisHandler.getServiceRegistrations, nil)).Methods("GET") //Public
 	tpsSubrouter.HandleFunc("/auth-keys", we.wrapFunc(we.tpsApisHandler.getAuthKeys, nil)).Methods("GET")                //Public
 
+	// tpsSubrouter.HandleFunc("/service-account", we.wrapFunc(we.tpsApisHandler.getServiceAccountParams, nil)).Methods("POST") //Public
 	tpsSubrouter.HandleFunc("/access-token", we.wrapFunc(we.tpsApisHandler.getServiceAccessToken, nil)).Methods("POST") //Public
 	///
 
