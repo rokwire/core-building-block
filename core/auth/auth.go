1b9bcc9d110bae83d5225063d00a77143beaf0f7
Stephen Hurwit <10472810+shurwit@users.noreply.github.com>
2022-05-16 09:42:47 -0500

    [ID-129] Prepare the project to become open source (#497)
    * update readme
    
    * add codeowners file
    
    * add license file
    
    * add code of conduct file
    
    * add contributing file
    
    * add issue templates
    
    * add pull request template
    
    * add standard license headers to all source files
    
    * set up detect secrets
    
    * remove unused code
    
    * update changelog
    
    * add missing secret baseline

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Board of Trustees of the University of Illinois.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package auth
 
 import (
@@ -913,9 +927,7 @@ func (a *Auth) findAccountAuthTypeByID(account *model.Account, accountAuthTypeID
 	if err != nil || authType == nil {
 		return nil, errors.WrapErrorAction(logutils.ActionLoadCache, typeAuthType, logutils.StringArgs(accountAuthType.AuthType.ID), err)
 	}
-	if authType == nil {
 
-	}
 	accountAuthType.AuthType = *authType
 
 	if accountAuthType.Credential != nil {
