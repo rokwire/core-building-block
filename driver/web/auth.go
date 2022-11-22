7826b59b7e191e3feb41648d52cccf7a9f93f45c
Ryan Oberlander <reoberlander2@gmail.com>
2022-11-22 03:09:27 -0600

    remove individual web auth entities, use claimsCheck functions

--- driver/web/auth.go
+++ driver/web/auth.go
@@ -15,22 +15,11 @@
 package web
 
 import (
-	"net/http"
-
 	"github.com/rokwire/core-auth-library-go/v2/authorization"
 	"github.com/rokwire/core-auth-library-go/v2/authservice"
 	"github.com/rokwire/core-auth-library-go/v2/tokenauth"
 	"github.com/rokwire/logging-library-go/v2/errors"
 	"github.com/rokwire/logging-library-go/v2/logutils"
-)
-
-const (
-	typeCheckScope                    logutils.MessageActionType = "checking scope"
-	typeCheckSystemAuthRequestToken   logutils.MessageActionType = "checking system auth"
-	typeCheckTPsAuthRequestToken      logutils.MessageActionType = "checking tps auth"
-	typeCheckBBsAuthRequestToken      logutils.MessageActionType = "checking bbs auth"
-	typeCheckAdminAuthRequestToken    logutils.MessageActionType = "checking admin auth"
-	typeCheckServicesAuthRequestToken logutils.MessageActionType = "checking services auth"
 )
 
 // Auth handler
@@ -53,7 +42,7 @@ func NewAuth(serviceID string, serviceRegManager *authservice.ServiceRegManager)
 
 	adminAuth, err := newAdminAuth(serviceRegManager)
 	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionStart, "admin auth", nil, err)
+		return nil, errors.WrapErrorAction(logutils.ActionCreate, "admin auth", nil, err)
 	}
 	adminHandlers := tokenauth.NewHandlers(adminAuth)
 
@@ -65,19 +54,19 @@ func NewAuth(serviceID string, serviceRegManager *authservice.ServiceRegManager)
 
 	bbsAuth, err := newBBsAuth(serviceRegManager)
 	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionStart, "bbs auth", nil, err)
+		return nil, errors.WrapErrorAction(logutils.ActionCreate, "bbs auth", nil, err)
 	}
 	bbsHandlers := tokenauth.NewHandlers(bbsAuth)
 
 	tpsAuth, err := newTPsAuth(serviceRegManager)
 	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionStart, "tps auth", nil, err)
+		return nil, errors.WrapErrorAction(logutils.ActionCreate, "tps auth", nil, err)
 	}
 	tpsHandlers := tokenauth.NewHandlers(tpsAuth)
 
 	systemAuth, err := newSystemAuth(serviceRegManager)
 	if err != nil {
-		return nil, errors.WrapErrorAction(logutils.ActionStart, "auth handler", nil, err)
+		return nil, errors.WrapErrorAction(logutils.ActionCreate, "auth handler", nil, err)
 	}
 	systemHandlers := tokenauth.NewHandlers(systemAuth)
 
@@ -86,87 +75,52 @@ func NewAuth(serviceID string, serviceRegManager *authservice.ServiceRegManager)
 	return &auth, nil
 }
 
-// ServicesAuth entity
-type ServicesAuth struct {
-	tokenAuth *tokenauth.TokenAuth
-}
+// ServicesAuth
 
-// Check checks the token in the provided request
-func (auth *ServicesAuth) Check(req *http.Request) (int, *tokenauth.Claims, error) {
-	claims, err := auth.tokenAuth.CheckRequestTokens(req)
-	if err != nil {
-		return http.StatusUnauthorized, nil, errors.WrapErrorAction(typeCheckServicesAuthRequestToken, logutils.TypeToken, nil, err)
-	}
-
+func servicesAuthClaimsCheck(claims *tokenauth.Claims) error {
 	if claims.Admin {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "admin claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "admin claim", nil)
 	}
 	if claims.System {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "system claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "system claim", nil)
 	}
-
-	err = auth.tokenAuth.AuthorizeRequestScope(claims, req)
-	if err != nil {
-		return http.StatusForbidden, nil, errors.WrapErrorAction(typeCheckScope, logutils.TypeRequest, nil, err)
-	}
-
-	return http.StatusOK, claims, nil
+	return nil
 }
 
-// GetTokenAuth exposes the TokenAuth for the handler
-func (auth *ServicesAuth) GetTokenAuth() *tokenauth.TokenAuth {
-	return auth.tokenAuth
-}
-
-func newServicesAuth(serviceRegManager *authservice.ServiceRegManager, serviceID string) (*ServicesAuth, error) {
+func newServicesAuth(serviceRegManager *authservice.ServiceRegManager, serviceID string) (*tokenauth.ScopeHandler, error) {
 	servicesScopeAuth := authorization.NewCasbinScopeAuthorization("driver/web/authorization_services_policy.csv", serviceID)
 	servicesPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_services_policy.csv")
 
 	servicesTokenAuth, err := tokenauth.NewTokenAuth(true, serviceRegManager, servicesPermissionAuth, servicesScopeAuth)
-
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for servicesAuth", nil, err)
 	}
 
-	auth := ServicesAuth{tokenAuth: servicesTokenAuth}
+	auth := tokenauth.NewScopeHandler(*servicesTokenAuth, servicesAuthClaimsCheck)
 	return &auth, nil
 }
 
-// AdminAuth entity
-type AdminAuth struct {
-	tokenAuth *tokenauth.TokenAuth
-}
+// AdminAuth
 
-// Check checks the token in the provided request
-func (auth *AdminAuth) Check(req *http.Request) (int, *tokenauth.Claims, error) {
-	claims, err := auth.tokenAuth.CheckRequestTokens(req)
-	if err != nil {
-		return http.StatusUnauthorized, nil, errors.WrapErrorAction(typeCheckAdminAuthRequestToken, logutils.TypeToken, nil, err)
-	}
-
+func adminAuthClaimsCheck(claims *tokenauth.Claims) error {
 	if !claims.Admin {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "admin claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "admin claim", nil)
 	}
-
-	return http.StatusOK, claims, nil
+	return nil
 }
 
-// GetTokenAuth exposes the TokenAuth for the handler
-func (auth *AdminAuth) GetTokenAuth() *tokenauth.TokenAuth {
-	return auth.tokenAuth
-}
-
-func newAdminAuth(serviceRegManager *authservice.ServiceRegManager) (*AdminAuth, error) {
+func newAdminAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.StandardHandler, error) {
 	adminPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_admin_policy.csv")
 	adminTokenAuth, err := tokenauth.NewTokenAuth(true, serviceRegManager, adminPermissionAuth, nil)
-
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for adminAuth", nil, err)
 	}
 
-	auth := AdminAuth{tokenAuth: adminTokenAuth}
+	auth := tokenauth.NewStandardHandler(*adminTokenAuth, adminAuthClaimsCheck)
 	return &auth, nil
 }
+
+// EncAuth
 
 func newEncAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.StandardHandler, error) {
 	encPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_enc_policy.csv")
@@ -175,122 +129,72 @@ func newEncAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.St
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for encAuth", nil, err)
 	}
 
-	auth := tokenauth.NewStandardHandler(*encTokenAuth)
+	auth := tokenauth.NewStandardHandler(*encTokenAuth, nil)
 	return &auth, nil
 }
 
-// BBsAuth entity
-type BBsAuth struct {
-	tokenAuth *tokenauth.TokenAuth
-}
+// BBsAuth
 
-// Check checks the token in the provided request
-func (auth *BBsAuth) Check(req *http.Request) (int, *tokenauth.Claims, error) {
-	claims, err := auth.tokenAuth.CheckRequestTokens(req)
-	if err != nil {
-		return http.StatusUnauthorized, nil, errors.WrapErrorAction(typeCheckBBsAuthRequestToken, logutils.TypeToken, nil, err)
-	}
-
+func bbsAuthClaimsCheck(claims *tokenauth.Claims) error {
 	if !claims.Service {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "service claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "service claim", nil)
 	}
-
 	if !claims.FirstParty {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "first party claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "first party claim", nil)
 	}
-
-	return http.StatusOK, claims, nil
+	return nil
 }
 
-// GetTokenAuth exposes the TokenAuth for the handler
-func (auth *BBsAuth) GetTokenAuth() *tokenauth.TokenAuth {
-	return auth.tokenAuth
-}
-
-func newBBsAuth(serviceRegManager *authservice.ServiceRegManager) (*BBsAuth, error) {
+func newBBsAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.StandardHandler, error) {
 	bbsPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_bbs_policy.csv")
 	bbsTokenAuth, err := tokenauth.NewTokenAuth(true, serviceRegManager, bbsPermissionAuth, nil)
-
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for bbsAuth", nil, err)
 	}
 
-	auth := BBsAuth{tokenAuth: bbsTokenAuth}
+	auth := tokenauth.NewStandardHandler(*bbsTokenAuth, bbsAuthClaimsCheck)
 	return &auth, nil
 }
 
-// TPsAuth entity
-type TPsAuth struct {
-	tokenAuth *tokenauth.TokenAuth
-}
+// TPsAuth
 
-// Check checks the token in the provided request
-func (auth *TPsAuth) Check(req *http.Request) (int, *tokenauth.Claims, error) {
-	claims, err := auth.tokenAuth.CheckRequestTokens(req)
-	if err != nil {
-		return http.StatusUnauthorized, nil, errors.WrapErrorAction(typeCheckTPsAuthRequestToken, logutils.TypeToken, nil, err)
-	}
-
+func tpsAuthClaimsCheck(claims *tokenauth.Claims) error {
 	if !claims.Service {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "service claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "service claim", nil)
 	}
-
 	if claims.FirstParty {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "first party claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "first party claim", nil)
 	}
-
-	return http.StatusOK, claims, nil
+	return nil
 }
 
-// GetTokenAuth exposes the TokenAuth for the handler
-func (auth *TPsAuth) GetTokenAuth() *tokenauth.TokenAuth {
-	return auth.tokenAuth
-}
-
-func newTPsAuth(serviceRegManager *authservice.ServiceRegManager) (*TPsAuth, error) {
+func newTPsAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.StandardHandler, error) {
 	tpsPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_tps_policy.csv")
 	tpsTokenAuth, err := tokenauth.NewTokenAuth(true, serviceRegManager, tpsPermissionAuth, nil)
-
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for tpsAuth", nil, err)
 	}
 
-	auth := TPsAuth{tokenAuth: tpsTokenAuth}
+	auth := tokenauth.NewStandardHandler(*tpsTokenAuth, tpsAuthClaimsCheck)
 	return &auth, nil
 }
 
-// SystemAuth entity
-type SystemAuth struct {
-	tokenAuth *tokenauth.TokenAuth
-}
+// SystemAuth
 
-// Check checks the token in the provided request
-func (auth *SystemAuth) Check(req *http.Request) (int, *tokenauth.Claims, error) {
-	claims, err := auth.tokenAuth.CheckRequestTokens(req)
-	if err != nil {
-		return http.StatusUnauthorized, nil, errors.WrapErrorAction(typeCheckSystemAuthRequestToken, logutils.TypeToken, nil, err)
-	}
-
+func systemAuthClaimsCheck(claims *tokenauth.Claims) error {
 	if !claims.System {
-		return http.StatusUnauthorized, nil, errors.ErrorData(logutils.StatusInvalid, "system claim", nil)
+		return errors.ErrorData(logutils.StatusInvalid, "system claim", nil)
 	}
-
-	return http.StatusOK, claims, nil
+	return nil
 }
 
-// GetTokenAuth exposes the TokenAuth for the handler
-func (auth *SystemAuth) GetTokenAuth() *tokenauth.TokenAuth {
-	return auth.tokenAuth
-}
-
-func newSystemAuth(serviceRegManager *authservice.ServiceRegManager) (*SystemAuth, error) {
+func newSystemAuth(serviceRegManager *authservice.ServiceRegManager) (*tokenauth.StandardHandler, error) {
 	systemPermissionAuth := authorization.NewCasbinStringAuthorization("driver/web/authorization_system_policy.csv")
 	systemTokenAuth, err := tokenauth.NewTokenAuth(true, serviceRegManager, systemPermissionAuth, nil)
-
 	if err != nil {
 		return nil, errors.WrapErrorAction(logutils.ActionStart, "token auth for systemAuth", nil, err)
 	}
 
-	auth := SystemAuth{tokenAuth: systemTokenAuth}
+	auth := tokenauth.NewStandardHandler(*systemTokenAuth, systemAuthClaimsCheck)
 	return &auth, nil
 }
