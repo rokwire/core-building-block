51cbe184a3465858ce967ede22f39ee4b021cd9c
Stefan Vitanov <stefan@inabyte.com>
2023-11-24 08:42:17 +0200

    add Apps to the conversion

--- driver/web/conversions_user.go
+++ driver/web/conversions_user.go
@@ -132,7 +132,10 @@ func partialAccountToDef(item model.Account, params map[string]interface{}) *Def
 		}
 	}
 
-	return &Def.PartialAccount{Id: &item.ID, Anonymous: item.Anonymous, AppId: item.AppOrg.Application.ID, OrgId: item.AppOrg.Organization.ID, FirstName: item.Profile.FirstName,
+	//apps
+	apps := appsToDef(as)
+
+	return &Def.PartialAccount{Id: &item.ID, Apps: &apps, Anonymous: item.Anonymous, AppId: item.AppOrg.Application.ID, OrgId: item.AppOrg.Organization.ID, FirstName: item.Profile.FirstName,
 		LastName: item.Profile.LastName, Username: username, System: &item.AppOrg.Organization.System, Permissions: permissions, Roles: roles, Groups: groups,
 		Privacy: privacy, Verified: &item.Verified, Scopes: &scopes, SystemConfigs: systemConfigs, AuthTypes: authTypes, DateCreated: &dateCreated,
 		DateUpdated: dateUpdated, Params: paramsData, ExternalIds: &externalIds}
