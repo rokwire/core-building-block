3ae3e8442dba32ee44f8a576aa373dffd19b288f
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2024-04-30 20:55:25 +0300

    [ID-710] Admin app account creation update (#711)
    * Update CHANGELOG.md
    
    * Mock external login for quick development
    
    * Has admin app access - in progress
    
    * Has admin app access - in progress
    
    * Implement hasAdminAppAccess function
    
    * Remove TODO comment
    
    * Revert commented code
    
    * In progress
    
    * Add membership - in progress
    
    * More stuff
    
    * Revert commented mock
    
    * Update to go1.22
    
    ---------
    
    Co-authored-by: Petyo Stoyanov <petyo.stoyanov@inabit.eu>

--- core/model/application.go
+++ core/model/application.go
@@ -388,6 +388,8 @@ type IdentityProviderSetting struct {
 
 	Roles  map[string]string `bson:"roles"`  //map[identity_provider_role]app_role_id
 	Groups map[string]string `bson:"groups"` //map[identity_provider_group]app_group_id
+
+	AdminAppAccessRoles []string `bson:"admin_app_access_roles"` //list with the identity provider roles which are approved for admin app account creation
 }
 
 // LoginsSessionsSetting represents logins sessions setting for an organization in an application
